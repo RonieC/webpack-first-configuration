38c634ceb3727cffbb510c4443c525ffe8bf66ee
Saul Crespo <saul.crespo@trueforce.com>
2018-04-15 17:26:00 -0400

    story/WP-config:more dependencies to load html pluging

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 const config = {
     context: path.resolve(__dirname, 'src'),
     entry: {
@@ -8,7 +11,42 @@ const config = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: './assets/js/[name].bundle.js'
-    }
+    },
+    module: {
+        rules: [
+            //babel-loader
+            {
+                test: /\.js$/,
+                include: /src/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['env']
+                    }
+                }
+            },
+            //html-loader
+            {
+                test: /\.html$/,
+                use: ['html-loader']
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            template: 'index.html'
+        })
+    ],
+    devServer: {
+        contentBase: path.resolve(__dirname, "dist/assets/media"),
+        stats: 'errors-only',
+        open: true,
+        port: 12000,
+        compress: true
+    },
+    devtool: 'inline-source-map'
 };
 
 module.exports = config;
